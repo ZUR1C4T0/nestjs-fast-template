27b702ba805f08887dc51a59f74ad5bb0dc2da17
ZUR1C4T0 <ivanulloque19@gmail.com>
2024-02-08 00:13:47 -0500

    format code

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	);
 
-  // start the server
-  const port = +process.env.PORT || 3000;
-  const address = '0.0.0.0';
-  await app.listen(port, address);
+	// start the server
+	const port = +process.env.PORT || 3000;
+	const address = "0.0.0.0";
+	await app.listen(port, address);
 }
 bootstrap();

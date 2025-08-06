614a713a1924bb9afd3a281564c249437050504b
Ivan Ulloque <95107566+ZUR1C4T0@users.noreply.github.com>
2025-08-06 16:06:32 +0000

    feat: Configure BiomeJS and enhance project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [ConfigModule.forRoot({ isGlobal: true })],
 	controllers: [AppController],
 	providers: [AppService],
 })
